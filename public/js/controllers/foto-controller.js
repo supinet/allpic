61d93796158b212ef196d169261efaa6603edef1
supinet <fernando.supi@gmail.com>
2017-04-12 21:41:01 -0400

    adicao de servico com  para isolar codigo

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,4 +1,4 @@
-angular.module('alurapic').controller('FotoController', function($scope, recursoFoto, $routeParams) {
+angular.module('alurapic').controller('FotoController', function($scope, cadastroDeFotos, recursoFoto, $routeParams) {
 
 	$scope.foto = {};
 	$scope.mensagem = '';
@@ -20,23 +20,32 @@ angular.module('alurapic').controller('FotoController', function($scope, recurso
 
 	$scope.submeter = function() {
 		if($scope.formulario.$valid){
-			if($scope.foto._id) {
+			// if($scope.foto._id) {
 
-				recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto, function(){
-					$scope.mensagem = 'A foto ' + $scope.foto.titulo + ' alterada com sucesso' ;
-				}, function(erro) {
-					console.log(erro);
-					$scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo; 
-				});
-			} else {
-				recursoFoto.save($scope.foto, function() {
-					$scope.foto = {};
-					$scope.mensagem = 'Foto incluída com sucesso';
-				}, function(erro) {
-					$scope.mensagem = 'Não foi possível incluir a Foto';
-					console.log(erro);
-				});
-			}
+			// 	recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto, function(){
+			// 		$scope.mensagem = 'A foto ' + $scope.foto.titulo + ' alterada com sucesso' ;
+			// 	}, function(erro) {
+			// 		console.log(erro);
+			// 		$scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo; 
+			// 	});
+			// } else {
+			// 	recursoFoto.save($scope.foto, function() {
+			// 		$scope.foto = {};
+			// 		$scope.mensagem = 'Foto incluída com sucesso';
+			// 	}, function(erro) {
+			// 		$scope.mensagem = 'Não foi possível incluir a Foto';
+			// 		console.log(erro);
+			// 	});
+			// }
+
+			cadastroDeFotos.cadastrar($scope.foto)
+			.then(function(dados){
+				$scope.mensagem = dados.mensagem;
+				if (dados.inclusao) $scope.foto = {};
+			})
+			.catch(function(dados){
+				$scope.mensagem = dados.mensagem;
+			})
 		}
 	};
 });
